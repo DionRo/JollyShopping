b52d38f2d7d0c58d42f0cbf328f643478e8e66ef
Unknown <d223013@edu.rocwb.nl>
2018-03-22 21:06:17 +0100

    Markup showOrder (No Function added YET)

--- resources/views/admin/showOrder.blade.php
+++ resources/views/admin/showOrder.blade.php
@@ -2,23 +2,45 @@
 
 @section('content')
     <div class="details">
-        {{dd($orders)}}
         <div class="container">
             <div class="catalogus">
-                <h2 style="font-size: 40px;">Uw winkelwagen ()</h2>
+                <div class="status" style="display: flex;">
+                    <form style="margin-right: 10px;" action="" method="POST">
+                        {{csrf_field()}}
+                        {{method_field('put')}}
+                        <input type="submit" value="Status ontvangen == 0 ">
+                    </form>
+                    <form style="margin-right: 10px;" action="" method="POST">
+                        {{csrf_field()}}
+                        {{method_field('put')}}
+                        <input type="submit" value="Status Betaald == 1">
+                    </form>
+                    <form style="margin-right: 10px;" action="" method="POST">
+                        {{csrf_field()}}
+                        {{method_field('put')}}
+                        <input type="submit" value="Status verstuurd == 2">
+                    </form>
+                    <form style="margin-right: 10px;" action="" method="POST">
+                        {{csrf_field()}}
+                        {{method_field('put')}}
+                        <input type="submit" value="Status afgerond == 3">
+                    </form>
+                </div>
                 @foreach($orders as $order)
+                    <h2 style="font-size: 40px;">Ordernummer #{{$order->orderId}}</h2>
+                    <p>Besteld door</p>
                     <div style="padding: 10px 20px; margin: 10px 0; background-color: #ffffff; border-radius: 20px;">
                         <div class="flex">
-                            <img src="{{$product['item']['picture']}}" alt="" style="width: 200px; height: 200px;">
+                            <img src="" alt="hier moet een afbeelding" style="width: 200px; height: 200px;">
                             <ul>
-                                <li style="font-size: 25px; font-weight: bold;">{{$product['item']['name']}}</li>
-                                <li style="font-size: 20px">Aantal: {{$product['qty']}}</li>
-                                @if($product['qty'] > 1)
-                                    <li style="font-size: 20px">Prijs per stuk:&#8364 {{$product['item']['price']}}</li>
-                                @endif
-                                <li style="font-size: 20px">Totaal:&#8364 {{$product['price']}}</li>
+                                <li style="font-size: 25px; font-weight: bold;">Naam Product</li>
+                                <li style="font-size: 20px">Aantal: {{$order->amountOrder}}</li>
+                                <li style="font-size: 20px">Prijs per stuk:&#8364</li>
+                                <li style="font-size: 20px">Totaal:&#8364 {{$order->totalPrice}}</li>
                             </ul>
                         </div>
+                        <p>Uw huidige orderstatus is {{$order->isProcessed}}</p>
+                        <p> {{$order->specify}} </p>
                     </div>
                 @endforeach
             </div>
